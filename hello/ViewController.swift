8a8b428dd5fbd0a55ad8080ccfba564b42dacfab
Muntasir Shuaib <muntasir.shuaib@yahoo.com>
2017-04-01 15:23:22 +0300

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Muntasir on 01/04/2017.
+//  Copyright © 2017 Muntasir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
